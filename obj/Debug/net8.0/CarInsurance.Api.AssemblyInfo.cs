4b45f2d6954063584b5936472b4e9a81b1f31a5b
Andrei <andreigeorgian33@gmail.com>
2025-08-28 19:27:26 +0300

    ControlersHttp modified

--- obj/Debug/net8.0/CarInsurance.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/CarInsurance.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarInsurance.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32766cd6152e7a51ca14919df0d6c27464af86de")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarInsurance.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarInsurance.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
